f1382493111bfdb1723d81a659b410ef3ed0dd76
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2020-12-16 12:33:59 +0000

    Car now moves

--- godot/Main.gd
+++ godot/Main.gd
@@ -15,6 +15,6 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process_physics(delta):
+func _physics_process(delta):
 	$Camera.look_at($VehicleTest.translation, UP)
 	pass
