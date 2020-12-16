6361194b4fed18037913fabcfd7a93582bdd1330
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2020-12-16 12:22:07 +0000

    Created car

---
+++ godot/Main.gd
@@ -0,0 +1,20 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var UP : Vector3
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	UP.x = 0
+	UP.y = 1
+	UP.z = 0
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process_physics(delta):
+	$Camera.look_at($VehicleTest.translation, UP)
+	pass
---
+++ godot/VehicleBody.gd
@@ -0,0 +1,29 @@
+class_name vehicle extends VehicleBody
+
+const STEER_SPEED = 1
+const STEER_LIMIT = 0.4
+
+var steer_target = 0
+
+export var engine_force_value = 40
+
+func _physics_process(delta):
+	var fwd_mps = transform.basis.xform_inv(linear_velocity).x
+	
+	steer_target = Input.get_action_strength("turn_left") - Input.get_action_strength("turn_right")
+	steer_target *= STEER_LIMIT
+	
+	if true or Input.is_action_pressed("accelerate"):
+		engine_force = engine_force_value
+	else:
+		engine_force = 0
+	
+	if Input.is_action_pressed("reverse"):
+		if (fwd_mps >= -1):
+			engine_force = -engine_force_value
+		else:
+			brake = 1
+	else:
+		brake = 0.0
+	
+	steering = move_toward(steering, steer_target, STEER_SPEED * delta)
